a97309e61b19498939e5cb6cf1f3b70dc7fa2430
Max Wallace <maxfield.wallace@gmail.com>
2017-06-19 15:03:48 -0400

    Don't write to every file, print the ones that changed

--- regex-remove-from-files.hs
+++ regex-remove-from-files.hs
@@ -1,6 +1,7 @@
 import System.Environment
 import System.Directory
 import Data.List
+import Control.Monad
 
 import Text.Regex
 import Text.Regex.PCRE
@@ -9,14 +10,33 @@ main = do
   regex:filepaths <- getArgs
 
   putStrLn ("Regex: " ++ regex)
-  putStrLn ("Files: " ++ (intercalate ", " filepaths))
+  putStrLn ((formatFilepaths filepaths) ++ "\n")
 
+  putStrLn "Modified:"
   mapM_ (replaceFile regex) filepaths
+  -- putStrLn "Modified " ++ (show (length modifiedFiles)) ++ " files:"
+  -- putStrLn (intercalate "\n" modifiedFiles)
 
 replaceFile regex path = do
   content <- readFile path
-  writeFile (path ++ ".bak") (applyRegex regex content)
-  renameFile (path ++ ".bak") path
+  let newContent = applyRegex regex content
+
+  if content == newContent
+  then return ()
+  else modifyFile path newContent
 
 applyRegex regex content =
   subRegex (mkRegex regex) content ""
+
+formatFilepaths filepaths =
+  "Files " ++
+  "(" ++ (show (length filepaths)) ++ " total): " ++
+  (if (length filepaths) > 3
+    then (intercalate ", " (take 3 filepaths)) ++ " ..."
+    else (intercalate ", " filepaths)
+  )
+
+modifyFile path newContent = do
+  writeFile (path ++ ".bak") newContent
+  renameFile (path ++ ".bak") path
+  putStrLn path
